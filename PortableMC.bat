18f996518a86bee6fd4c4ac506ae1b9f7fe5f3c2
Jonáš Rosecký <rosecky.jonas@gmail.com>
2024-04-17 12:40:37 +0200

    Added servers.dat

--- PortableMC.bat
+++ PortableMC.bat
@@ -30,7 +30,7 @@ color 0c
 echo Minecraft launcher not found! Downloading...
 timeout /t 3 /nobreak > NUL
 powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://launcher.mojang.com/download/Minecraft.exe' '%CD%\GameData\Minecraft\Minecraft.exe'"
-:: powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://www.dropbox.com/s/ly1111w198fnvt0/servers.dat?dl=1' '%CD%\GameData\AppData\.minecraft\servers.dat'"
+powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://raw.githubusercontent.com/roseckyj/PortableMC/main/data/servers.dat' '%CD%\GameData\AppData\.minecraft\servers.dat'"
 goto launch
  
 :end
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PortableMC
 
 This is an adapted version of the [PortableMC](https://github.com/portablemc/portablemc) repository for the needs of user testing at FI MUNI.
+
+## Installation
+
+Download the [PortableMC.bat](PortableMC.bat) file and run it. The script will download the latest version of Minecraft launcher and run it.
